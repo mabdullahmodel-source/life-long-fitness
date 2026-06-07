b210c44128c96f770bfe61bc550785a4eccff035
mabdullahmodel-source <m.abdullahmodel@gmail.com>
2026-06-07 17:02:49 +0400

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,16 @@ export default defineConfig({
   plugins: [react()],
   base: './', 
 })
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path' // Add this import
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  base: './',
+})
